21aecf8e81327afd62374a4f5d3efa3ff5e87eea
xiaozhi <1516441157@qq.com>
2023-04-28 15:42:44 +0800

    完善vector

--- vector/vector/main.cpp
+++ vector/vector/main.cpp
@@ -16,26 +16,8 @@ void my_printf(vector v)
 
 int main()
 {
-	/*vector v;
-	for (int i = 0; i < 10; i++)
-	{
-		v.push_back(i);
-	}
-	my_printf(v);
 	
 
-	vector test(v);
-	my_printf(test);*/
-
-	int i = 0;
-	while ("skadljdakls")
-	{
-		i++;
-		cout << i << endl;
-		if (i > 5)
-		{
-			break;
-		}
-	}
+	
 
 }
