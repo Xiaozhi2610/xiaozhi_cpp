afaabeef14cec28ae4eff93fe641fbf8108c309f
xiaozhi <2979528251@qq.com>
2023-04-30 18:17:17 +0800

    数据库添加功能完善

--- test/数据库连接测试/main.cpp
+++ test/数据库连接测试/main.cpp
@@ -7,11 +7,37 @@
 
 using namespace std;
 
+char host[] = "127.0.0.1";
+char user[] = "root";
+char pw[] = "Xz261025";
+char database_name[] = "xiaozhi";
+int port = 3306;
+string temp;
 
 int main()
 {
 	show_con();
-	my_sql xiazhi_mysql;
+	int con = 0;
+	cin >> con;
+	while (con != 1 && con != 2)
+	{
+		cout << "请重新输入连接方式: ";
+		cin >> con;
+	}
+	if (con == 2)
+	{
+		std::cout << "请输入需要连接的主机IP地址: ";
+		std::cin >> host;
+		std::cout << "请输入需要登录的用户: ";
+		std::cin >> user;
+		std::cout << "请输入用户的登录密码: ";
+		std::cin >> pw;
+		std::cout << "请输入要连接的数据库名称: ";
+		std::cin >> database_name;
+		std::cout << "请输入要连接的端口号: ";
+		std::cin >> port;
+	}
+	my_sql xiazhi_mysql(host,user,pw,database_name,port);
 	myprint_menu();
 	int flag = 1;
 	int choose = 0;
